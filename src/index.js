add0b479aeb6f43ffed0391646eeed4da0455120
oscar <oscar_sagitario@hotmail.com>
2022-03-02 00:55:03 -0600

    Commit Exitoso

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-//Commit exitoso
+//Commit exitosoo
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
